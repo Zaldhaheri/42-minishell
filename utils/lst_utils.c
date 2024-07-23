85ea902d6c5058ad6dedf4571438fe79dcbb2bf8
NaserAlkhateri <59799187+NaserAlkhateri@users.noreply.github.com>
2024-07-23 15:21:22 -0400

    Few stuff add tot test testShell.c

--- utils/lst_utils.c
+++ utils/lst_utils.c
@@ -7,7 +7,18 @@ t_token	*ft_lstnew(char *word)
 	if (!node)
 		return (NULL);
 	node->content = word;
+	node->arg = NULL;
 	node->next = NULL;
+	return (node);
+}
+
+t_token	*ft_lstlast(t_token *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next != NULL)
+		lst = lst->next;
+	return (lst);
 }
 
 void	ft_lstadd_back(t_token **lst, t_token *new)
@@ -26,12 +37,22 @@ void	ft_lstadd_back(t_token **lst, t_token *new)
 void	ft_lstclear(t_token **lst)
 {
 	t_token	*temp;
+	t_args	*temp_sub;
 
 	if (*lst)
 	{
 		while (*lst != NULL)
 		{
 			temp = *lst;
+			
+			while ((*lst)->arg)
+			{
+				temp_sub = (*lst)->arg;
+				(*lst)->arg = (*lst)->arg->next;
+				free(temp_sub->content);
+				free(temp_sub);
+			}
+			
 			*lst = (*lst)->next;
 			free(temp->content);
 			free(temp);
--- utils/utils1.c
+++ utils/utils1.c
@@ -60,3 +60,20 @@ char	*ft_strrchr(const char *str, int c)
 	}
 	return (NULL);
 }
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t		i;
+
+	i = 0;
+	if (dstsize > 0)
+	{
+		while (src[i] && i < dstsize - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (ft_strlen(src));
+}
