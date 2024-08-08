38c4067eb55d42f736281c278c979d19935f8f3e
Naser Alkhateri <naser@alkhateri.com>
2024-08-08 21:11:41 +0400

    updated type

--- src/type.c
+++ src/type.c
@@ -5,47 +5,34 @@ static void	check_fd_type(t_token *temp)
 	if (temp->type)
 		return ;
 	if (temp->next && !ft_strncmp(temp->content,"<",1) && ft_strlen(temp->content) == 1)
-	{
 		temp->type = FD_IN;
-		temp->next->type = FILENAME;
-	}
 	else if(temp->next && !ft_strncmp(temp->content,">",1) && ft_strlen(temp->content) == 1)
-	{
 		temp->type = FD_OUT;
-		temp->next->type = FILENAME;
-	}
 	else if(!ft_strncmp(temp->content,">>",2))
-	{
 		temp->type = APPEND;
-		temp->next->type = FILENAME;
-	}
 	else if(!ft_strncmp(temp->content,"<<",2))
-	{
 		temp->type = HEREDOC;
-		temp->next->type = LIMITER;
-	}
 }
 
 static int	check_if_command(t_data *data, t_token *curr)
 {
 	t_token	*temp;
 	int		command_flag;
-	// int		pipe_flag;
 
 	if (curr->type)
 		return 2;
 	command_flag = 0;
-	// pipe_flag = 0;
 	temp = data->tokens;
 	while(temp && temp != curr)
 	{
-		if (temp->type == COMMAND || temp->type == BCOMMAND)
+		if (temp->type == COMMAND || temp->type == BCOMMAND || temp->type == FILENAME || temp->type == LIMITER)
 			command_flag = 1;
 		else if(temp->type == PIPE)
-		{
-			// pipe_flag = 1;
 			command_flag = 0;
-		}
+		else if(temp->type == FD_IN || temp->type == FD_OUT || temp->type == APPEND)
+		 	command_flag = 3;
+		else if(temp->type == HEREDOC)
+		 	 command_flag = 4;
 		temp = temp->next;
 	}
 	return(command_flag);
@@ -104,8 +91,12 @@ void	set_type(t_data *data)
 				temp->type = PIPE;
 			else if (check_if_command(data, temp) == 1)
 				temp->type = FLAG;
-			else if(!check_if_command(data, temp))
+			else if(check_if_command(data, temp) == 0)
 				temp->type = COMMAND;
+			else if (check_if_command(data, temp) == 3)
+				temp->type = FILENAME;
+			else if (check_if_command(data, temp) == 4)
+				temp->type = LIMITER;
 		}
 		temp = temp->next;
 	}
