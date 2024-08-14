744ca5f802d1140c6ef2fa809f799077a473a118
Naser Alkhateri <nalkhate@lab1r6s12.42abudhabi.ae>
2024-08-14 21:18:28 +0400

    ctrl-c handle

--- src/minishell.c
+++ src/minishell.c
@@ -21,9 +21,12 @@ void signal_handler(int signo)
 {
     	if (signo == SIGINT)
 	{
-		 rl_on_new_line();
-		 rl_redisplay();
-		//l_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+		ft_putstr_fd(" \n", 2);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
 		 
 	}
 
