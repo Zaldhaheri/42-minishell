f63b5760d0936433aa1e7c94716ebac1c827a89c
Naser Alkhateri <naser@alkhateri.com>
2024-08-16 19:57:11 +0400

    added cases for echo

--- src/bcomm/bcomm.c
+++ src/bcomm/bcomm.c
@@ -8,11 +8,12 @@ void b_echo(char **com)
     flag = 0;
     char *temp;
     int j;
+    int not_words = 1;
    ft_putstr_fd("BCOMM--->echo function\n",2);
     while(com[i] != NULL)
     {
         temp = com[i];
-        if (!ft_strncmp(temp, "-n", 2))
+        if (!ft_strncmp(temp, "-n", 2) && not_words == 1)
         {
             j = 1;
             while(temp && temp[j] == 'n')
@@ -21,6 +22,7 @@ void b_echo(char **com)
             }
             if (temp[j])
             {
+                not_words = 0;
                 printf("%s", temp);
                 if (com[i + 1])
                     printf(" ");
@@ -31,6 +33,7 @@ void b_echo(char **com)
 
         }else
         {
+            not_words = 0;
             printf("%s", temp);
             if (com[i + 1])
                 printf(" ");
