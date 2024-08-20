be0b4510833d2770279acccb62cd6b4c430ff400
zaldhaheri <zayed.ahmed123321@gmail.com>
2024-08-20 21:20:34 +0400

    minishell.h

--- inc/minishell.h
+++ inc/minishell.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell2.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zaldhahe <zaldhahe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/20 21:09:43 by zaldhahe          #+#    #+#             */
+/*   Updated: 2024/08/20 21:18:15 by zaldhahe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
@@ -15,32 +27,7 @@
 #include <string.h>
 #include <termios.h>
 
-#define WORD 0
-#define COMMAND 11 //execve
-#define FD_IN 12
-#define FD_OUT 13
-#define APPEND 14
-#define HEREDOC 15
-#define FILENAME 16
-#define LIMITER 17
-#define PIPE 18
-#define DQUOTES 19
-#define SQUOTES 20
-#define DOLLAR 21
-#define DECLARE 22
-#define BCOMMAND 23 //builtin
-#define FLAG 24
-#define NO_FD -10
-
-#define RESET   "\033[0m"
-#define BLACK   "\033[30m"
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
-#define BLUE    "\033[34m"
-#define MAGENTA "\033[35m"
-#define CYAN    "\033[36m"
-#define WHITE   "\033[37m"
+#include "define.h"
 
 typedef struct s_token t_token;
 typedef struct s_env t_env;
