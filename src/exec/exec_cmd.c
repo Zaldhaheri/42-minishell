00f7d8fe5d3de50f0ffef84e75d5ef4680f1e9d4
Naser Alkhateri <nalkhate@lab1r3s3.42abudhabi.ae>
2024-08-21 09:47:59 +0400

    fixed in out logic and created new file

---
+++ src/exec/exec_cmd.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_cmd.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nalkhate <nalkhate@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/21 09:43:08 by nalkhate          #+#    #+#             */
+/*   Updated: 2024/08/21 09:43:24 by nalkhate         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "executor.h"
+
+void	exec_cmd(t_command *cmd, t_data *data)
+{
+	pid_t			pid;
+	t_child_params	params;
+
+	params.fd_in = STDIN_FILENO;
+	params.fd_out = STDOUT_FILENO;
+	params.is_first = 1;
+	params.fd[0] = -10;
+	params.fd[1] = -10;
+	while (cmd)
+	{
+		if (cmd->next)
+			create_pipe(&params);
+		if (!cmd->next && params.is_first && cmd->is_bcommand)
+			parent_pid(cmd, &params, data);
+		else
+		{
+			pid = fork();
+			if (pid == -1)
+				return ((perror("fork"), exit(EXIT_FAILURE)));
+			if (pid == 0)
+				start_child(cmd, data, &params);
+			else
+				parent_pid(cmd, &params, data);
+		}
+		cmd = cmd->next;
+	}
+	if (params.fd_in != STDIN_FILENO)
+		close(params.fd_in);
+	waitpid(pid, &(data->status), 0);
+}
--- src/exec/exec_utils.c
+++ src/exec/exec_utils.c
@@ -6,7 +6,7 @@
 /*   By: nalkhate <nalkhate@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/11 16:24:22 by nalkhate          #+#    #+#             */
-/*   Updated: 2024/08/20 23:21:19 by nalkhate         ###   ########.fr       */
+/*   Updated: 2024/08/21 09:44:01 by nalkhate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,35 +64,41 @@ void	start_child(t_command *cmd, t_data *data, t_child_params *params)
 		dup2(cmd->cmd_fd, STDOUT_FILENO);
 	else if (cmd->next)
 		dup2(params->fd[1], STDOUT_FILENO);
-	if (!params->is_first)
-	{
-		if (cmd->fd_type == FD_IN)
-			dup2(cmd->cmd_fd, STDIN_FILENO);
-		else
-			dup2(params->fd_in, STDIN_FILENO);
-	}
+	if (cmd->fd_type == FD_IN)
+		dup2(cmd->cmd_fd, STDIN_FILENO);
+	else if (!params->is_first)
+		dup2(params->fd[0], STDIN_FILENO);
 	if (cmd->next)
 		close(params->fd[0]);
 	close(params->fd[1]);
 	exec_child(cmd, data, params);
 }
 
+static void	bcomm_parent(t_command *cmd, t_data *data)
+{
+	int	saved_stdout;
+
+	if (cmd->fd_type == FD_OUT || cmd->fd_type == APPEND)
+		saved_stdout = dup(STDOUT_FILENO);
+	if (cmd->fd_type == FD_OUT || cmd->fd_type == APPEND)
+		dup2(cmd->cmd_fd, STDOUT_FILENO);
+	bcomm_exec(cmd, data);
+	if (cmd->fd_type == FD_OUT || cmd->fd_type == APPEND)
+	{
+		dup2(saved_stdout, STDOUT_FILENO);
+		close(saved_stdout);
+	}
+	if (cmd->cmd_fd == -1)
+		data->status = 1;
+	else
+		data->status = 0;
+}
+
 void	parent_pid(t_command *cmd, t_child_params	*params, t_data *data)
 {
-	// int	saved_stdout;
-
 	if (cmd->is_bcommand && !cmd->next && params->is_first)
 	{
-		//saved_stdout = dup(STDOUT_FILENO);
-		if (cmd->fd_type == FD_OUT || cmd->fd_type == APPEND)
-			dup2(cmd->cmd_fd, STDOUT_FILENO);
-		bcomm_exec(cmd, data);
-		//dup2(saved_stdout, STDOUT_FILENO);
-		// close(saved_stdout);
-		if (cmd->cmd_fd == -1)
-			data->status = 1;
-		else
-			data->status = 0;
+		bcomm_parent(cmd, data);
 		return ;
 	}
 	if (cmd->next)
@@ -108,38 +114,4 @@ void	parent_pid(t_command *cmd, t_child_params	*params, t_data *data)
 	if (cmd->cmd_fd > 0)
 		close(cmd->cmd_fd);
 	params->is_first = 0;
-}
-
-void	exec_cmd(t_command *cmd, t_data *data)
-{
-	pid_t			pid;
-	t_child_params	params;
-
-	params.fd_in = STDIN_FILENO;
-	params.fd_out = STDOUT_FILENO;
-	params.is_first = 1;
-	params.fd[0] = -10;
-	params.fd[1] = -10;
-	while (cmd)
-	{
-		if (cmd->next)
-			create_pipe(&params);
-		if (!cmd->next && params.is_first && cmd->is_bcommand)
-			parent_pid(cmd, &params, data);
-		else
-		{
-			pid = fork();
-			if (pid == -1)
-				return ((perror("fork"), exit(EXIT_FAILURE)));
-			if (pid == 0)
-				start_child(cmd, data, &params);
-			else
-				parent_pid(cmd, &params, data);
-		}
-		cmd = cmd->next;
-	}
-	if (params.fd_in != STDIN_FILENO)
-		close(params.fd_in);
-	if (!params.is_first)
-		waitpid(pid, &(data->status), 0);
 }
