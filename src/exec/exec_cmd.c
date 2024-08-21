29f1dd64cbe89e9f422ba963fa7e1a65f900dbcf
Naser Alkhateri <nalkhate@lab1r3s3.42abudhabi.ae>
2024-08-21 09:52:02 +0400

    norme exec_cmd

--- src/exec/exec_cmd.c
+++ src/exec/exec_cmd.c
@@ -6,22 +6,27 @@
 /*   By: nalkhate <nalkhate@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 09:43:08 by nalkhate          #+#    #+#             */
-/*   Updated: 2024/08/21 09:43:24 by nalkhate         ###   ########.fr       */
+/*   Updated: 2024/08/21 09:50:04 by nalkhate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
+
+static void	params_init(t_child_params	*params)
+{
+	params->fd_in = STDIN_FILENO;
+	params->fd_out = STDOUT_FILENO;
+	params->is_first = 1;
+	params->fd[0] = -10;
+	params->fd[1] = -10;
+}
 
 void	exec_cmd(t_command *cmd, t_data *data)
 {
 	pid_t			pid;
 	t_child_params	params;
 
-	params.fd_in = STDIN_FILENO;
-	params.fd_out = STDOUT_FILENO;
-	params.is_first = 1;
-	params.fd[0] = -10;
-	params.fd[1] = -10;
+	params_init(&params);
 	while (cmd)
 	{
 		if (cmd->next)
